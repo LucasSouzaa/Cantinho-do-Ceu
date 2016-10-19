fa258c92f81be9b50f3b7b4105dbe72e4d25ac02
Luiz Ricardo <luiz.proffi@gmail.com>
2016-10-19 16:38:00 -0200

    teste d.fox

--- application/views/home.php
+++ application/views/home.php
@@ -10,7 +10,7 @@
 <br/><br/>
 
  <div class="btn btn-danger">									
-	<big><i class="fa fa-exclamation-triangle"></i></big>  O Remédio Dipirona está em falta!! <i class="fa fa-eye"></i>									
+	<big><i class="fa fa-exclamation-triangle"></i></big>  O Remédio Dipirona está em falta! <i class="fa fa-eye"></i>									
  </div>
  <br/><br/>
  
