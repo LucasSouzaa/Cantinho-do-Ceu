571b3c9d1ee659ddad4fb43e08645d5b43297991
Mariana B. Marques <mariana.bianchi.marques@gmail.com>
2016-10-22 22:07:37 -0200

    modificação dos alertas

--- application/views/administrator/dashboard.php
+++ application/views/administrator/dashboard.php
@@ -1,8 +1,35 @@
- <div class="btn btn-danger">									
-	<big><i class="fa fa-exclamation-triangle"></i></big>  O Remédio Dipirona está em falta! <i class="fa fa-eye"></i>									
- </div>
- <br/><br/>
+
+<div class="col-lg-3 col-md-4 col-sm-4">
+    <div class="panel panel-stat1 msg-success">									
+        <div class="title">
+            <big><i class="fa fa-exclamation-triangle"></i></big> 
+            O Remédio Dipirona está em falta! <i class="fa fa-eye"></i> 
+        </div>									
+    </div>
+</div> 
  
- <div class="btn btn-danger">									
-	<big><i class="fa fa-exclamation-triangle"></i></big>  O Remédio Loratradina do lote 3 faltam 8 dias para acabar a validades! <i class="fa fa-eye"></i>			
-</div>
+ <div class="col-lg-3 col-md-4 col-sm-4">
+    <div class="panel panel-stat1 msg-info">									
+        <div class="title">
+            <big><i class="fa fa-exclamation-triangle"></i></big>
+            O Remédio Loratradina do lote 3 faltam 8 dias para acabar a validades! <i class="fa fa-eye"></i>
+        </div>        
+   </div>
+ </div>
+
+<div class="col-lg-3 col-md-4 col-sm-4">
+    <div class="panel panel-stat1 msg-warning">									
+        <div class="title">
+            <big><i class="fa fa-exclamation-triangle"></i></big>  O Remédio Loratradina do lote 3 faltam 8 dias para acabar a validades! <i class="fa fa-eye"></i>
+        </div>			
+   </div>
+ </div>
+
+<div class="col-lg-3 col-md-4 col-sm-4">
+    <div class="panel panel-stat1 msg-danger">
+        <div class="title">
+            <big><i class="fa fa-exclamation-triangle"></i></big>  O Remédio Loratradina do lote 3 faltam 8 dias para acabar a validades! <i class="fa fa-eye"></i>
+        </div>
+        
+   </div>
+ </div>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -9,7 +9,7 @@
 		 <link rel="stylesheet" href="<?= $this->config->base_url('assets/css/bootstrap.min.css') ?>" />
 		 <link rel="stylesheet" href="<?= $this->config->base_url('assets/css/AdminLTE.min.css') ?>" />
 		 <link rel="stylesheet" href="<?= $this->config->base_url('assets/css/skins/_all-skins.min.css') ?>" />
-
+                 <link rel="stylesheet" href="<?= $this->config->base_url('assets/css/style.css') ?>" />
 		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 		<title>Cantinho do Céu</title>
